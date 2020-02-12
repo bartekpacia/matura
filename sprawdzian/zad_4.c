2cceca5205c8eb0a319772dff8782b9dba092d87
Bartek Pacia <barpac02@gmail.com>
2020-02-12 10:58:03 +0100

    zrobiono zad 4 ze sprawdzianu

---
+++ sprawdzian/zad_4.c
@@ -0,0 +1,28 @@
+/**
+ * Użytkownik podaje 2 liczby rzeczywiste. Program wyświetla ich iloczyn.
+ */
+
+#include <stdio.h>
+
+int main() {
+    int wiek;
+    printf("Proszę podać wiek: ");
+    scanf("%i", &wiek);
+
+    if (wiek <= 0) {
+        printf("Niewłaściwy wiek!\n");
+        getchar();
+        return 0;
+    }
+
+    if (wiek <= 11) {
+        printf("Kategoria: dzieci do 11 lat\n");
+    } else if (wiek >= 12 && wiek <= 16) {
+        printf("Kategoria: młodzież od 12 lat\n");
+    } else if (wiek >= 16) {
+        printf("Kategoria: młodzież od 16 lat\n");
+    }
+
+    getchar();
+    return 0;
+}
