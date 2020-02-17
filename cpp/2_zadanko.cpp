99c77f32e17a029aca12d8f367daa232498a82b4
Bartek Pacia <barpac02@gmail.com>
2020-02-17 08:32:29 +0100

    zrobiono zadanko z C++

---
+++ cpp/2_zadanko.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int k = 0;
+    std::cout << "Podaj liczbę: " << std::endl;
+    std::cin >> k;
+
+    for (int i = 1; i <= k; i++) {
+        if (i % 6 == 0) {
+            std::cout << "Znaleziono liczbę " << i << " podzielną przez 6"
+                      << std::endl;
+        }
+    }
+
+    getchar();
+
+    return 0;
+}
