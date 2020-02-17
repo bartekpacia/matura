cf783095ef74720376683da81c29069dd0832746
Bartek Pacia <barpac02@gmail.com>
2020-02-17 08:44:06 +0100

    bardziej ogólna podzielność

---
+++ cpp/3_podzielnosc.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+/**
+ * Wszystkie liczby dodatnie mniejsze od k podzielne przez n.
+ * k i n to liczby naturalne podawane przez użytkownika.
+ */
+
+int main() {
+    int k, n = 0;
+    std::cout << "Podaj k: " << std::endl;
+    std::cin >> k;
+    std::cout << "Podaj n: " << std::endl;
+    std::cin >> n;
+
+    if (k < 1 || n < 1) {
+        std::cout << "Podane niewłaściwe dane" << std::endl;
+        return 0;
+    }
+
+    if (k < n) {
+        std::cout << "Brak dzielników liczby " << n << " w przedziale <1; " << k
+                  << ">" << std::endl;
+    }
+
+    for (int i = 1; i <= k; i++) {
+        if (i % n == 0) {
+            std::cout << "Znaleziono liczbę " << i << " podzielną przez " << n
+                      << std::endl;
+        }
+    }
+
+    return 0;
+}
