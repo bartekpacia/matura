e306c6639c87c2a72b400bcb66f85e81c6b644f7
Bartek Pacia <barpac02@gmail.com>
2020-02-19 10:59:10 +0100

    naprawiono zadanie domowe

--- cpp/5_zadanie_domowe_1.cpp
+++ cpp/5_zadanie_domowe_1.cpp
@@ -1,19 +1,20 @@
 /**
  * Napisz program, który po wczytaniu dwóch liczb całkowitych w i k,
  * oznaczających liczbę wierszy i kolumn, gdzie są wstawione gwiazdki, obliczy
- * bok największego kwadratu, który można wypełnić tymi gwiazdkami. Program
+ * bok największego kwadratu, który można zbudować z tych gwiazdek. Program
  * powinien również wypisać liczbę niewykorzystanych gwiazdek (nie będą
  * potrzebne do wypełnienia kwadratu).
  */
 
+#include <math.h>
 #include <iostream>
 #include <string>
 
 int main() {
     int wiersze, kolumny = 0;
-    std::cout << "Podaj liczbę wierszy: " << std::endl;
+    std::cout << "Podaj liczbę wierszy: ";
     std::cin >> wiersze;
-    std::cout << "Podaj liczbę kolumn: " << std::endl;
+    std::cout << "Podaj liczbę kolumn: ";
     std::cin >> kolumny;
 
     if (!(wiersze > 0 && kolumny > 0)) {
@@ -22,20 +23,13 @@ int main() {
         return 0;
     }
 
-    int najwiekszy_bok = 0;
-    if (wiersze <= kolumny) {
-        najwiekszy_bok = wiersze;
-    } else {
-        najwiekszy_bok = kolumny;
-    }
-
     int gwiazdki = wiersze * kolumny;
-    int gwiazdki_w_kwadracie = najwiekszy_bok * najwiekszy_bok;
-    int niewykorzystane_gwiazdki = gwiazdki - gwiazdki_w_kwadracie;
+    int bkwadrat = floor(sqrt(gwiazdki));
+    int reszta = gwiazdki - (bkwadrat * bkwadrat);
 
-    std::cout << "Bok największego kwadratu: " << najwiekszy_bok << std::endl;
-    std::cout << "Liczba niewykorzystanych gwiazdek: "
-              << niewykorzystane_gwiazdki << std::endl;
+    std::cout << "Bok najwiekszego kwadratu do zbudowania z tych gwiazdek to "
+              << bkwadrat << std::endl;
+    std::cout << "Reszta po zbudowaniu kwadratu to " << reszta << std::endl;
 
     getchar();
     return 0;
