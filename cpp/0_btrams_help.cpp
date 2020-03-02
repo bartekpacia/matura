ea89266575676642961e54efbd0a6a33e825b560
Bartek Pacia <barpac02@gmail.com>
2020-03-02 23:04:34 +0100

    dodano oryginalne zadanie Bartka

--- cpp/0_btrams_help.cpp
+++ cpp/0_btrams_help.cpp
@@ -5,7 +5,31 @@ using namespace std;
 
 int main() {
     int n;
-    if (i == n - j) {
-        tablica[i][j] = przekatna[1][j];
+    int tablica[n][n];
+    int g = 100;
+    int d = 0;
+    int przekatna[1][n];
+    cout << "podaj rozmiar kwadratu ";
+    cin >> n;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            tablica[i][j] = rand() % (g - d + 1);
+            cout << tablica[i][j] << " ";
+
+            if (i == j) {
+                tablica[i][j] = przekatna[0][j];
+            }
+            if (i == n - j) {
+                tablica[i][j] = przekatna[1][j];
+            }
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << przekatna[0][i] << " ";
     }
 }
