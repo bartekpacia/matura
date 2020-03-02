e7dd91df09ffcf19d0a376b520c8bf532ed03ccb
Bartek Pacia <barpac02@gmail.com>
2020-03-02 16:33:35 +0100

    zrobiono 26 z ksiazeczki

---
+++ cpp/26_najwieksze_liczby_w_kolumnie.cpp
@@ -0,0 +1,48 @@
+/**
+ * Napisz program, który wczyta liczbę n, wypełni tablicę n x n wylosowanymi
+ * liczbami dwucyfrowymi, a następnie wypisze największe liczby z kolejnych
+ * kolumn tablicy.
+ */
+#include <iostream>
+
+int main() {
+    int p = 10;
+    int k = 99;
+    int rozmiar = 0;
+
+    std::cout << "Podaj rozmiar tablicy: ";
+    std::cin >> rozmiar;
+
+    int tab[rozmiar][rozmiar];
+
+    srand(time(NULL));
+
+    for (int i = 0; i < rozmiar; i++) {
+        for (int j = 0; j < rozmiar; j++) {
+            tab[i][j] = rand() % (k - p + 1) + p;
+        }
+    }
+
+    for (int i = 0; i < rozmiar; i++) {
+        for (int j = 0; j < rozmiar; j++) {
+            std::cout << tab[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    for (int i = 0; i < rozmiar; i++) {
+        int max = INT_MIN;
+
+        for (int j = 0; j < rozmiar; j++) {
+            int obecny = tab[j][i];
+
+            if (obecny > max) {
+                max = obecny;
+            }
+        }
+
+        std::cout << "najwiekszy w kolumnie " << i << ": " << max << std::endl;
+    }
+
+    return 0;
+}
