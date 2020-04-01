dbf353cafc910966c278d32314b0e5d44308561a
Bartek Pacia <barpac02@gmail.com>
2020-04-01 16:33:59 +0200

    dodano szlaczek costam costam

--- cpp/0038_szlaczek.cpp
+++ cpp/0038_szlaczek.cpp
@@ -2,7 +2,26 @@
 
 using namespace std;
 
+int szlaczek(char znak, int liczba_znakow) {
+    for (int i = 0; i < liczba_znakow; i++) {
+        cout << znak;
+    }
+
+    cout << endl;
+    return znak;  // zwracamy kod ASCII znaku (występuje rzutowanie char -> int)
+}
+
 int main(int argc, char const *argv[]) {
-    cout << "Nie chce mi się tego robić" << endl;
+    int dlugosc;
+    char znaczek;
+    cout << "Podaj znaczek: ";
+    cin >> znaczek;
+
+    cout << "Podaj długość szlaczka: ";
+    cin >> dlugosc;
+
+    int kod_znaku = szlaczek(znaczek, dlugosc);
+    cout << "Kod znaku w szlaczku: " << kod_znaku << endl;
+
     return 0;
 }
