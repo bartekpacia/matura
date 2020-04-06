f19b2682ec0acad4add821dca0129c95cabfcbf6
Bartek Pacia <barpac02@gmail.com>
2020-04-06 15:21:34 +0200

    dodano kulke

---
+++ cpp/0041_kula.cpp
@@ -0,0 +1,39 @@
+#include <math.h>
+
+#include <iostream>
+
+using namespace std;
+
+float oblicz_pole_kuli(int r) {
+    float pole = 4 * M_PI * r * r;
+    return pole;
+}
+
+float oblicz_objetosc_kuli(int r) {
+    float objetosc = (4 * M_PI * r * r * r) / 3;
+    return objetosc;
+}
+
+int main() {
+    int r;
+    while (true) {
+        cout << "Podaj promień kuli: ";
+        cin >> r;
+
+        if (r <= 0) {
+            cout << "Niepoprawna wartość promienia!" << endl;
+            continue;
+        } else {
+            break;
+        }
+    }
+
+    float pole = oblicz_pole_kuli(r);
+    float objetosc = oblicz_objetosc_kuli(r);
+
+    cout << "____________________________" << endl;
+    cout << "Pole powierzchni tej kuli to " << pole << endl;
+    cout << "Objetosc tej kuli to " << objetosc << endl;
+
+    return 0;
+}
