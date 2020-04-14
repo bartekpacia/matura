75bf2cc20c0ad46e3d94ed21de087799cf08ce4e
Bartek Pacia <barpac02@gmail.com>
2020-04-14 17:18:51 +0200

    naprawiono pierwsze

--- help/natalia_1.cpp
+++ help/natalia_1.cpp
@@ -1,6 +1,18 @@
 // Zadanie 1/86
 
 #include <iostream>
+
+using namespace std;
+
+int szlaczek(char znak, int liczba_znakow) {
+    for (int i = 0; i < liczba_znakow; i++) {
+        cout << znak;
+    }
+
+    cout << endl;
+    return znak;
+}
+
 int main() {
     char zn;
     int d;
@@ -8,5 +20,6 @@ int main() {
     cin >> zn;
     cout << "Podaj długosć: ";
     cin >> d;
-    void szlaczek(zn, d) return;
+    szlaczek(zn, d);
+    return 0;
 }
