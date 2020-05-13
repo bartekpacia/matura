8a5bf87f9d7b9938692e26f1b17879e2a5743fee
Bartek Pacia <barpac02@gmail.com>
2020-05-13 10:43:52 +0200

    zrobiłem to chyba

--- cpp/struktury_danych/stos.cpp
+++ cpp/struktury_danych/stos.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 // element stosu
 struct ElementStosu {
-    int dana;
+    char dana;
     ElementStosu* nastepny;
 };
 
@@ -16,7 +16,7 @@ ElementStosu* gora(ElementStosu* S) { return S; }
 
 // dodaj element na szczyt stosu, wartość elementu czyli dana, jest parametrem
 // funkcji
-void dodaj(int wartosc, ElementStosu** S) {
+void dodaj(char wartosc, ElementStosu** S) {
     ElementStosu* elem = new ElementStosu;
 
     elem->dana = wartosc;  // tutaj wpisujemy wartość do danej; wartość to
@@ -42,14 +42,14 @@ int main() {
     // szczyt stosu; na poczatku stos jest pusty
     ElementStosu* stos = NULL;
 
-    int i;
-
     // dodajemy 10 elementów całkowitych na stos (liczby od 1 do 10)
-
-    for (i = 1; i <= 10; i++) {
-        dodaj(i, &stos);
+    for (int i = 1; i <= 5; i++) {
+        char znak_od_uzytkownika;
+        cin >> znak_od_uzytkownika;
+        dodaj(znak_od_uzytkownika, &stos);
     }
 
+    cout << "Wyświetlanie stosu (poniżej): " << endl;
     // czytamy i wyświetlamy elementy ze szczytu stosu i usuwamy je ze stosu
     while (!czyPusty(stos)) {
         cout << gora(stos)->dana << endl;
