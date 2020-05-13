438b5f5fa5d0c93741feb3c57e1ee76c3366217a
Bartek Pacia <barpac02@gmail.com>
2020-05-14 01:10:46 +0200

    ulepszyłem stos
    - wywalone couty do debugowania
    - możliwe że naprawiłem meory leak

--- cpp/struktury_danych/stos_oop.cpp
+++ cpp/struktury_danych/stos_oop.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <stack>
 
 using namespace std;
 
@@ -7,7 +6,6 @@ struct StackElement {
     char value;
     StackElement* next;
 
-    // StackElement(char value) : value(value) {}
     StackElement(char value, StackElement* next) : value(value), next(next) {}
 };
 
@@ -17,46 +15,29 @@ struct Stack {
     bool isEmpty() { return top == NULL; }
 
     void push(char value) {
-        cout << "begin push()" << endl;
         StackElement* newElement = new StackElement(value, top);
-        cout << "middle push()" << endl;
 
-        // == if top points to something
-        if (top) {
-            cout << "top points to something." << endl;
-        } else {
-            cout << "top doens't point to anything!" << endl;
-        }
         top = newElement;
-
-        cout << "end push()" << endl;
     }
 
     void remove() {
         StackElement* toBeReturned = top;
 
         top = top->next;
-        // FIXME: Memory leak
-        // delete toBeReturned;
+        // FIXME: Memory leak???
+        delete toBeReturned;
     }
 };
 
 int main() {
     Stack* stack = new Stack();
+    cout << "Adres stosu: " << &stack << endl;
 
-    // for (int i = 1; i <= 5; i++) {
-    //     char znak_od_uzytkownika;
-    //     cin >> znak_od_uzytkownika;
-    //     push(znak_od_uzytkownika, &stack);
-    // }
-
-    cout << &stack << endl;
-
-    stack->push('p');
-    stack->push('a');
-    stack->push('c');
-    stack->push('i');
-    stack->push('a');
+    for (int i = 1; i <= 5; i++) {
+        char znak_od_uzytkownika;
+        cin >> znak_od_uzytkownika;
+        stack->push(znak_od_uzytkownika);
+    }
 
     cout << "Displaying content of the stack: " << endl;
 
