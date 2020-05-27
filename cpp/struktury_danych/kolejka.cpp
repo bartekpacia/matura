90b15f0ff22f5bdd0adc250d738e3af21d6ed9b6
Bartek Pacia <barpac02@gmail.com>
2020-05-27 13:57:06 +0200

    fix names

--- cpp/struktury_danych/kolejka.cpp
+++
@@ -1,7 +0,0 @@
-struct Queue {
-    int size;
-    Queue(int size) { this->size = size; }
-};
-
-// nie chce mi sie
-// wystarczy zrobić #include <queue> i voila
--- cpp/struktury_danych/tree.cpp
+++ cpp/struktury_danych/tree.cpp
@@ -1,1 +1,13 @@
-// c++ to śmieć
+// 1. c++ to śmieć
+// 2. nie widze sensu zeby to pisac
+
+#include <iostream>
+
+struct Node {
+    int data;
+
+    Node* left;
+    Node* right;
+};
+
+int main() { return 0; }
