944eee036ec0db94faccf21281ed5648f0f78634
Bartek Pacia <barpac02@gmail.com>
2020-09-30 19:37:38 +0200

    snek rulez

--- matury/2015/zad41.py
+++ matury/2015/zad41.py
@@ -1,14 +1,14 @@
 with open("dane/liczby.txt") as f:
-  lines = f.readlines()
+  lines = [l.strip() for l in f.readlines()]
 
-wynik4 = open("wynik4.txt", "w")
+wynik41 = open("wynik41.txt", "w")
 
 count = 0
 for line in lines:
   zeros = 0
   ones = 0
 
-  for char in line.strip():
+  for char in line:
     if char == "0":
       zeros += 1
     elif char == "1":
@@ -17,4 +17,5 @@ for line in lines:
   if zeros > ones:
     count += 1
 
-wynik4.write(f"zadanie 4.1: {count} liczb ma wiecej zer niz jedynek")
+wynik41.write(f"zadanie 4.1: {count} liczb ma wiecej zer niz jedynek")
+wynik41.close()
---
+++ matury/2015/zad42.py
@@ -0,0 +1,17 @@
+with open("dane/liczby.txt") as f:
+  lines = [l.strip() for l in f.readlines()]
+
+wynik42 = open("wynik42.txt", "w")
+
+div_2 = 0
+div_8 = 0
+for line in lines:
+
+  if line[-1] == "0":
+    div_2 += 1
+
+  if line[-1] == "0" and line[-2] == "0" and line[-3] == "0":
+    div_8 += 1
+
+wynik42.write(f"zadanie 4.2: {div_2} liczb podzielnych przez 2, {div_8} liczb podzielnych przez 8")
+wynik42.close()
