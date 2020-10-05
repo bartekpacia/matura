34fc8837f5104262b4f60179b79f0591920681f2
Bartek Pacia <barpac02@gmail.com>
2020-10-05 12:23:53 +0200

    dokonczono 6.2 / 2017

--- matury/2017/zad62.py
+++ matury/2017/zad62.py
@@ -7,7 +7,23 @@ for line in input_file:
 
   bitmap.append(pixels_int)
 
+symmetric_rows_count = 0
 for i in range(len(bitmap)):
+  symmetric = True
   for j in range(len(bitmap[i])):
-    pixel = bitmap[i][j]
+    left_pixel = bitmap[i][j]
+    right_pixel = bitmap[i][319 - j]
+
+    if left_pixel != right_pixel:
+      symmetric = False
+      break
+
+  if symmetric:
+    print(f"symetryczny wiersz {i=}")
+    symmetric_rows_count += 1
+
+
+rows_that_can_be_removed = len(bitmap) - symmetric_rows_count
+
+print(f"{rows_that_can_be_removed=}")
 
