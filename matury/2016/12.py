74ab7c75c33d3d78c15a8963ee23e749093ceabb
Bartek Pacia <barpac02@gmail.com>
2020-11-12 18:28:08 +0100

    test 2016/1.2

---
+++ matury/2016/12.py
@@ -0,0 +1,21 @@
+# checking my "whiteboard solution"
+
+def skojarzona(a: int):
+  suma_dzielnikow_a = 0
+  for i in range(1, (a // 2) + 1):
+    if a % i == 0:
+      suma_dzielnikow_a += i
+
+  suma_dzielnikow_b = 0
+  b = suma_dzielnikow_a - 1
+  for i in range(1, (b // 2) + 1):
+    if b % i == 0:
+      suma_dzielnikow_b += i
+
+  if suma_dzielnikow_b == a + 1:
+    print(f"{b=}")
+  else:
+    print("NIE")
+
+for i in range(1, 100):
+  skojarzona(i)
