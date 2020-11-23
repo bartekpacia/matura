52ff9c5aec6f504f30ca3cd3db14abe616188689
Bartek Pacia <barpac02@gmail.com>
2020-11-23 10:12:01 +0100

    idk

---
+++ algorytmy/fibonacci1.py
@@ -0,0 +1,18 @@
+def fib(n: int) -> int:
+  n1, n2 = 0, 1
+  n3 = 0
+  for k in range(2, n + 1):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+
+  return n3
+    
+
+print(fib(0))
+print(fib(1))
+print(fib(2))
+print(fib(3))
+print(fib(4))
+print(fib(5))
+print(fib(6))
