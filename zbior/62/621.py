762ea45ad81eab7f6e97b9ff82007ee410274e1f
Bartek Pacia <barpac02@gmail.com>
2021-01-08 22:58:21 +0100

    do 62.1

---
+++ zbior/62/621.py
@@ -0,0 +1,17 @@
+with open("liczby1.txt") as f:
+  liczby1 = [int(l, 8) for l in f.readlines()]
+
+# with open("liczby2.txt") as f:
+#   liczby2 = [int(l, 10) for l in f.readlines()]
+
+low = float("inf")
+high = -1
+for i, num in enumerate(liczby1):
+  if num < low:
+    low = num
+  
+  if num > high:
+    high = num
+
+print("min: " + str(oct(low))[2:])
+print("max: " + str(oct(high))[2:])
