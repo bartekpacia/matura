706cb3fb2efe1a92cfa24282f493ecbd1f5d13b0
Bartek Pacia <barpac02@gmail.com>
2021-01-09 00:21:35 +0100

    working on 63.3

---
+++ zbior/63/633.py
@@ -0,0 +1,25 @@
+with open("ciagi.txt", "r") as f:
+  ciagi = [int(l, 2) for l in f.readlines()]
+
+polpierwsze = []
+
+def jest_polpierwsza(num: int) -> bool:
+  for i in range((num // 2) + 1):
+    for j in range((num // 2) + 1):
+      pass
+      
+
+
+count_polpierwsze = 0
+
+low = 0
+if polpierwsze:
+  low = min(polpierwsze)
+
+high = 0
+if polpierwsze:
+  high = max(polpierwsze)
+
+print(f"{count_polpierwsze=}")
+print(f"min polpierwsza: {low}")
+print(f"max polpierwsza: {high}")
