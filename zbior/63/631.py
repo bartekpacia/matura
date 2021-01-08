cf05c86c41e4b087b28bb1565fb439ec7e1ee85d
Bartek Pacia <barpac02@gmail.com>
2021-01-09 00:08:38 +0100

    do 63.1 and 63.2

---
+++ zbior/63/631.py
@@ -0,0 +1,23 @@
+with open("ciagi.txt", "r") as f:
+  ciagi = [l.strip() for l in f.readlines()]
+
+def jest_dwucykliczny(ciag: str) -> bool:
+  if len(ciag) % 2 != 0:
+    return False
+
+  first_half = ciag[:(len(ciag) // 2)]
+  second_half = ciag[len(ciag) // 2:]
+
+  if first_half == second_half:
+    return True
+  return False
+
+count = 0
+for ciag in ciagi:
+  if jest_dwucykliczny(ciag):
+    count += 1
+    print(ciag)
+
+print(f"znaleziono {count} podciagow")
+
+
