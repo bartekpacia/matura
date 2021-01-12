a056781ba93d1568dd7caf5d74519fa343f04e98
Bartek Pacia <barpac02@gmail.com>
2021-01-12 18:33:21 +0100

    do 641

---
+++ zbior/64/641.py
@@ -0,0 +1,41 @@
+from typing import List
+
+file = open("dane_obrazki.txt", "r")
+
+max_zeros = 0
+zeros = 0
+ones = 0
+i = 0
+
+rewersy = 0
+for line in file.readlines():
+  fmt_line = line.strip()[:-1]
+
+  if fmt_line == "":
+    if ones > zeros:
+      print(f"{i} jest rewersem")
+      rewersy += 1
+    else:
+      print(f"{i} NIE jest rewersem")
+
+    if zeros > max_zeros:
+      max_zeros = zeros
+
+    zeros = 0
+    ones = 0
+    i += 1
+    continue
+
+  for char in fmt_line:
+    if char == '1':
+      ones += 1
+    elif char == "0":
+      zeros += 1
+    else:
+      print("bad")
+  
+
+file.close()
+
+print(f"liczba rewersów: {rewersy}")
+print(f"najwięcej czarnych pikseli: {max_zeros}")
