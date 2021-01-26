69eda6f7323fca537f7a6564289facc27cd21b0f
Bartek Pacia <barpac02@gmail.com>
2021-01-26 19:02:47 +0100

    reformat 64

--- zbior/64/641.py
+++ zbior/64/641.py
@@ -1,5 +1,3 @@
-from typing import List
-
 file = open("dane_obrazki.txt", "r")
 
 zeros = 0
@@ -9,33 +7,30 @@ img_index = 0
 
 rewersy = 0
 for line_i, line in enumerate(file.readlines()):
-  fmt_line = line.strip()[:-1]
+    fmt_line = line.strip()[:-1]
 
-  if fmt_line == "":
-    print(f"empty line at {line_i+1}")
-    if ones > zeros:
-      rewersy += 1
+    if fmt_line == "":
+        if ones > zeros:
+            rewersy += 1
 
-    if ones > max_ones:
-      max_ones = ones
-      print(f"new max ones at line {line_i+1}")
+        if ones > max_ones:
+            max_ones = ones
 
-    zeros = 0
-    ones = 0
-    img_index += 1
-    continue
+        zeros = 0
+        ones = 0
+        img_index += 1
+        continue
 
-  elif len(fmt_line) != 20:
-    continue
+    elif len(fmt_line) != 20:
+        continue
 
-  for char in fmt_line:
-    if char == "1":
-      ones += 1
-    elif char == "0":
-      zeros += 1
-    else:
-      print("bad")
-  
+    for char in fmt_line:
+        if char == "1":
+            ones += 1
+        elif char == "0":
+            zeros += 1
+        else:
+            print("bad")
 
 file.close()
 
--- zbior/64/642.py
+++ zbior/64/642.py
@@ -2,63 +2,72 @@ from typing import List
 
 file = open("dane_obrazki.txt", "r")
 
+
 def is_recursive(image) -> bool:
-  if len(image) % 2 != 0 or len(image) != 20:
+    if len(image) % 2 != 0 or len(image) != 20:
+        return False
+
+    for row in image:
+        if len(row) != len(image):
+            return False
+
+    half = 10
+    part1: List[int][int] = []
+    part2: List[int][int] = []
+    part3: List[int][int] = []
+    part4: List[int][int] = []
+
+    for row_i, row in enumerate(image):
+        for col_i, _ in enumerate(row):
+            if row_i < half and col_i < half:
+                part1.append(row[:half])
+            if row_i < half and col_i >= half:
+                part2.append(row[half:])
+            if row_i >= half and col_i < half:
+                part3.append(row[:half])
+            if row_i >= half and col_i >= half:
+                part4.append(row[half:])
+
+    if part1 == part2 and part2 == part3 and part3 == part4:
+        return True
     return False
 
-  for row in image:
-    if len(row) != len(image):
-      return False
-  
-  half = 10
-  part1: List[int][int] = []
-  part2: List[int][int] = []
-  part3: List[int][int] = []
-  part4: List[int][int] = []
 
-  for row_i, row in enumerate(image):
-    for col_i, _ in enumerate(row):
-      if row_i < half and col_i < half:
-        part1.append(row[:half])
-      if row_i < half and col_i >= half:
-        part2.append(row[half:])
-      if row_i >= half and col_i < half:
-        part3.append(row[:half])
-      if row_i >= half and col_i >= half:
-        part4.append(row[half:])
-
-  if part1 == part2 and part2 == part3 and part3 == part4:
-    return True
-  return False
-
+recursive_imgs = []
 cur_img_i = 0
 cur_img = []
 cur_row_i = 0
 rec_count = 0
 for line in file.readlines():
-  fmt_line = line.strip()[:-1]
+    fmt_line = line.strip()[:-1]
 
-  if fmt_line == "":
-    if is_recursive(cur_img):
-      rec_count += 1
-    
-    cur_img = []
-    cur_row_i = 0
-    cur_img_i += 1
-    continue
+    if fmt_line == "":
+        if is_recursive(cur_img):
+            recursive_imgs.append(cur_img)
+            rec_count += 1
 
-  if len(fmt_line) != 20:
-    continue
+        cur_img = []
+        cur_row_i = 0
+        cur_img_i += 1
+        continue
 
-  # now the row is good for sure
-  cur_img.append([])
+    if len(fmt_line) != 20:
+        continue
 
-  for char in fmt_line:
-    cur_img[cur_row_i].append(int(char))
+    # now the row is good for sure
+    cur_img.append([])
 
-  cur_row_i += 1
-  
+    for char in fmt_line:
+        cur_img[cur_row_i].append(int(char))
+
+    cur_row_i += 1
 
 file.close()
 
 print(f"liczba obrazków rekurencyjnych: {rec_count}")
+print(f"pierwszy obrazek rekurencyjny:")
+first_img = recursive_imgs[0]
+for i, _ in enumerate(first_img):
+    for j in first_img[i]:
+        print(j, end="")
+    print()
