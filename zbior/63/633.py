094d68e17cf6b530708e205e6ea065b150f76d96
Bartek Pacia <barpac02@gmail.com>
2021-01-26 17:08:16 +0100

    do 633

--- zbior/63/633.py
+++ zbior/63/633.py
@@ -1,16 +1,50 @@
-with open("ciagi.txt", "r") as f:
-    ciagi = [int(l, 2) for l in f.readlines()]
+from typing import List
 
-polpierwsze = []
+with open("ciagi.txt", "r") as f:
+    ciagi = [int(line, 2) for line in f.readlines()]
+
+
+def sito(n: int) -> List[int]:
+    if n < 2:
+        return list()
+
+    arr = [True] * (n + 1)
+    i = 2
+    while i ** 2 < n:
+        if arr[i]:
+            j = i ** 2
+            while j <= n:
+                arr[j] = False
+                j += i
+        i += 1
+
+    result = list()
+    for i in range(2, n + 1):
+        if arr[i]:
+            result.append(i)
+
+    return result
+
+
+highest_num = 2 ** 18  # maksymalna długość ciągu
+wszystkie_pierwsze = sito(highest_num)
 
 
 def jest_polpierwsza(num: int) -> bool:
-    for i in range((num // 2) + 1):
-        for j in range((num // 2) + 1):
-            pass
+    for p in wszystkie_pierwsze:
+        if num % p == 0 and (num / p) in wszystkie_pierwsze:
+            return True
+
+    return False
 
 
-count_polpierwsze = 0
+polpierwsze = []
+
+for c in ciagi:
+    if jest_polpierwsza(c):
+        polpierwsze.append(c)
+
+count_polpierwsze = len(polpierwsze)
 
 low = 0
 if polpierwsze:
