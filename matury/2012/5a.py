004c361e862614d0519fa402fda8d655c90069ec
Bartek Pacia <barpac02@gmail.com>
2021-03-06 19:21:20 +0100

    do this shieeet (bbbb)

--- matury/2012/5a.py
+++ matury/2012/5a.py
@@ -1,48 +1,9 @@
-from typing import List
-
-
-def gen_triangle(n: int) -> List[List[int]]:
-    if n < 1:
-        print("error: n < 1")
-        return []
-
-    triangle = [[1]]
-
-    for i in range(1, n):
-        triangle.append([])
-
-        for j in range(i + 1):
-            ii = i - 1
-            jj = j - 1
-            if jj >= 0 and ii >= 0:
-                up_left = triangle[ii][jj]
-            else:
-                up_left = 0
-
-            if ii >= 0:
-                try:
-                    up = triangle[ii][j]
-                except IndexError:
-                    up = 0
-            else:
-                up = 0
-
-            triangle[i].append(up_left + up)
-
-    return triangle
-
+from triangle import gen_triangle
 
 my_triangle = gen_triangle(30)
 
 max_in_10 = max(my_triangle[9])
 max_in_20 = max(my_triangle[19])
 max_in_30 = max(my_triangle[29])
-
-for i, _ in enumerate(my_triangle):
-    print(f"row: {i + 1}", end="    ")
-    for j, _ in enumerate(my_triangle[i]):
-        print(my_triangle[i][j], end=" ")
-
-    print("")
 
 print(f"{max_in_10=}, {max_in_20=}, {max_in_30=}")
--- matury/2012/test.py
+++
@@ -1,4 +0,0 @@
-l = ["a", "b", "c"]
-
-for i, val in enumerate(l):
-    print(f"{i=}, {val=}")
