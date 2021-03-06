b5bd5bd93729f86abe0240e99e1bd4d96a30294a
Bartek Pacia <barpac02@gmail.com>
2021-03-06 19:07:34 +0100

    niiiice trujkont

--- matury/2012/5a.py
+++ matury/2012/5a.py
@@ -6,21 +6,36 @@ def gen_triangle(n: int) -> List[List[int]]:
         print("error: n < 1")
         return []
 
-    triangle = []
+    triangle = [[1]]
 
-    for i in range(n):
+    for i in range(1, n):
         triangle.append([])
-        row_len = i + 1
 
-        for j in range(row_len):
-            triangle[i].append(1)
+        for j in range(i + 1):
+            ii = i - 1
+            jj = j - 1
+            if jj >= 0 and ii >= 0:
+                up_left = triangle[ii][jj]
+            else:
+                up_left = 0
+
+            if ii >= 0:
+                try:
+                    up = triangle[ii][j]
+                except IndexError:
+                    up = 0
+            else:
+                up = 0
+
+            triangle[i].append(up_left + up)
 
     return triangle
 
 
 my_triangle = gen_triangle(10)
 for i, _ in enumerate(my_triangle):
+    print(f"row: {i + 1}", end="    ")
     for j, _ in enumerate(my_triangle[i]):
-        print(my_triangle[i][j], end="")
+        print(my_triangle[i][j], end=" ")
 
     print("")
