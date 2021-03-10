1a0e3e3c56b245da819e528f1ae6d57ae4e2b5fe
Bartek Pacia <barpac02@gmail.com>
2021-03-10 18:09:48 +0100

    do 663

---
+++ zbior/66/663.py
@@ -0,0 +1,28 @@
+from typing import Tuple
+
+
+def is_square_triangle(a: int, b: int, c: int) -> bool:
+    boki = [a, b, c]
+    przeciw = max(a, b, c)
+    boki.remove(przeciw)
+
+    if boki[0] ** 2 + boki[1] ** 2 == przeciw ** 2:
+        return True
+
+    return False
+
+
+with open("trojki.txt") as file:
+    last_square: Tuple[int, int, int] = None
+    for line in file:
+        a, b, c = map(int, line.strip().split())
+
+        current_square = (a, b, c)
+        if is_square_triangle(a, b, c):
+            if last_square:
+                print(*last_square)
+                print(*current_square)
+
+            last_square = current_square
+        else:
+            last_square = None
