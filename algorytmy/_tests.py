88d1a641a344e708f755021e4af03077fa4e9a30
Bartek Pacia <barpac02@gmail.com>
2021-03-23 19:10:32 +0100

    test bsearch

--- algorytmy/_tests.py
+++ algorytmy/_tests.py
@@ -1,5 +1,17 @@
+from bi import bsearch, bisect
+
 import unittest
+
 
 class TestAlgorithms(unittest.TestCase):
 
-    def test_nwd()
+    def test_bsearch(self):
+        arr = [0, 2, 4, 6, 9, 69]
+        self.assertEqual(bsearch(0, arr), 0)
+        self.assertEqual(bsearch(2, arr), 1)
+        self.assertEqual(bsearch(6, arr), 3)
+        self.assertEqual(bsearch(9, arr), 4)
+        self.assertEqual(bsearch(69, arr), 5)
+
+    def test_nwd(self):
+        pass
--- algorytmy/bi.py
+++ algorytmy/bi.py
@@ -1,6 +1,24 @@
-def bsearch():
-    pass
+from typing import List, Optional
 
+
+def bsearch(target: int, arr: List[int]) -> Optional[int]:
+    high = len(arr) - 1
+    low = 0
+
+    while low <= high:
+        mid = (high + low) // 2
+        guess = arr[mid]
+        if guess < target:
+            print(f"{guess=} < {target=}")
+            low = mid + 1
+        elif guess > target:
+            print(f"{guess=} > {target=}")
+            high = mid - 1
+        else:
+            print(f"{guess=} == {target=}")
+            return mid
+
+    return None
 
 def bisect():
     pass
