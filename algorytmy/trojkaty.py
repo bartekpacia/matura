d8d11169c2adbe425bd189ed779d719a8fb02771
Bartek Pacia <barpac02@gmail.com>
2021-03-24 11:00:30 +0100

    czy trojkat

--- algorytmy/trojkaty.py
+++ algorytmy/trojkaty.py
@@ -1,2 +1,12 @@
-def czy_trojkat(a: int, b: int, c: int) -> bool:
-    pass
+def czy_trojkat(a: float, b: float, c: float) -> bool:
+    bok_max = max(a, b, c)
+    bok_min = min(a, b, c)
+
+    if bok_min + b > bok_max:
+        return True
+
+    return False
+
+
+var = czy_trojkat(3, 4, 6)
+print(var)
