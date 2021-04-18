ed62076f15143bd5f746a5f85190985800902b59
Bartek Pacia <barpac02@gmail.com>
2021-04-18 13:50:37 +0200

    do 43

--- matury/xxxxpr_operon/zad43.py
+++ matury/xxxxpr_operon/zad43.py
@@ -1,9 +1,17 @@
-from utils import to_num_system
+from utils import to_num_system, is_palindrome
 
 with open("dane/dane.txt") as f:
     lines = [int(line.strip()) for line in f.readlines()]
 
-print(to_num_system(15, 16))
+palindromes = {}
+for num in lines:
+    for i in range(2, 17):
+        n = to_num_system(num, i)
+        if is_palindrome(n):
+            if not palindromes.get(i):
+                palindromes[i] = 1
+            else:
+                palindromes[i] += 1
 
-for i in range(2, 17):
-    print(to_num_system(100, i))
+for p, v in sorted(palindromes.items()):
+    print(p, v)
