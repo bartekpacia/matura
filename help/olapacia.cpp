6fdbbdea0c6b6a710378f22e90d467c9f91e64aa
Bartek Pacia <barpac02@gmail.com>
2021-05-27 21:23:19 +0200

    gówno

---
+++ help/olapacia.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char pierwsza_litera;
+  cout << "podaj pierwszą literę: ";
+  cin >> pierwsza_litera;
+
+  char druga_litera;
+  cout << "podaj drugą literę: ";
+  cin >> druga_litera;
+
+  cout << "pierwsza litera: " << pierwsza_litera << " "
+       << "druga litera: " << druga_litera << endl;
+
+  return 0;
+}
