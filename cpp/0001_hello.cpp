8a8bd8f309f32591e2fe6fa7c3d670afc01a3880
Szymon Bednorz <dsonyyy@gmail.com>
2021-07-02 22:14:14 +0200

    !matura

--- cpp/0001_hello.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, World from C++!" << std::endl;
-
-    getchar();
-    return 0;
-}
--- zbior/73/importer.py
+++
@@ -1,6 +0,0 @@
-def read_file() -> list[str]:
-    with open("tekst.txt") as f:
-        text = f.readline().strip()
-
-    words = text.split()
-    return words
